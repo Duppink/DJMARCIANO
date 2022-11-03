d150eebbc7340c02c748541a941793b3d750fca9
odafenix <68091954+odafenix@users.noreply.github.com>
2022-11-03 16:38:05 -0300

    Implementación inicial del sistema de escenas

---
+++ Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    // Funciones para acceder a distintas escenas.
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GoToIntroduction()
+    {
+        SceneManager.LoadScene("Introduction");
+    }
+
+    public void GoToBriefing()
+    {
+        SceneManager.LoadScene("Briefing");
+    }
+
+    public void GoToGameplay()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    // Función que nos permite salir del juego.
+    public void ExitGame()
+    {
+      Application.Quit();
+    }
+
+}
+
